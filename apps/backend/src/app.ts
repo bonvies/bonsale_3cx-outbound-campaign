1006c735bf98bb6873f0691d68c6d7fdf6e08f0d
Aruminiya <leo80103@gmail.com>
2026-04-30 10:13:41 +0800

    feat(outbound-campaign): redis added reconnectStrategy

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -401,7 +401,6 @@ httpServer.listen(PORT, async () => {
     if (ENABLE_OUTBOUND_CAMPAIGN) { // 只有在自動外播功能啟用時才建立 Redis 連線
       // 建立 Redis 連線，用於儲存外播專案狀態與撥號名單
       await initRedis();
-      logWithTimestamp({ isForce: true }, `🔴 Redis server is connected`);
     }
 
     if (ENABLE_CALL_SCHEDULE) { // 只有在語音通知功能啟用時才初始化資料庫與啟動相關服務
